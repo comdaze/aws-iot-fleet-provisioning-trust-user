cffeca300f4fcff524f8208a63735e9dbcb3b069
comdaze@163.com <comdaze@163.com>
2021-09-08 11:01:39 +0800

    add RaspiWiFI

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python env
+# coding:utf-8
+import socket
+
+ip_port = ('0.0.0.0', 8080)
+back_log = 10
+buffer_size = 1024
+
+
+def main():
+    webserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    webserver.bind(ip_port)
+    webserver.listen(back_log)
+
+    while True:
+        conn, addr = webserver.accept()
+        print(addr)
+        recvdata = conn.recv(buffer_size)
+
+        with open("/Users/zhihay/workspace/aws-iot-fleet-provisioning-trust-user/index.html","rb") as f:
+             data = f.read()
+             conn.sendall(bytes("HTTP/1.1 201 OK\r\n\r\n", "utf-8"))  
+             conn.sendall(data)
+
+        conn.close()
+
+
+if __name__ == '__main__':
+    main()
